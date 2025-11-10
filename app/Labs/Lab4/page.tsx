d879d31e5c2bd5741fb149088de53e5d927deb76
Adarsh Kolli <adarshkolli@Adarshs-MacBook-Pro.local>
2025-11-09 23:34:28 -0500

    Fixed build error

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -3,7 +3,8 @@
 import dynamic from 'next/dynamic';
 
 const Lab4 = dynamic(() => import('./index'), {
-  ssr: false
+  ssr: false,
+  loading: () => <div>Loading Lab 4...</div>
 });
 
 export default function Page() {
