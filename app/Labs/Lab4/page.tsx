328e6be3e246c289622637f5e3f43b35d59e701d
Adarsh Kolli <adarshkolli@Adarshs-MacBook-Pro.local>
2025-11-09 23:50:45 -0500

    commiting

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -1,7 +1,36 @@
 "use client"
 
-import Lab4 from "./index";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateElement";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
-export default function Page() {
-  return <Lab4 />;
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return (
+    <div id="wd-lab4">
+      <h2>Lab 4</h2>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples />
+    </div>
+  );
 }
